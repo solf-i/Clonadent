2f5473de79ee6b3dde0fd19f2948e01b3bd3a0c0
Sol Ferri <solferri@hotmail.com>
2021-10-01 10:51:44 -0300

    Actualización de contenido / CSS / JS

---
+++ js/main.js
@@ -0,0 +1,54 @@
+
+$(document).ready(function(){
+  
+$("#desplegar").click(function (e) {
+
+  e.preventDefault();
+
+  $(".m1__item").toggle();
+   
+});
+
+
+
+$("#desplegar2").click(function (e) {
+
+  e.preventDefault();
+
+  $(".m2__item").toggle();
+
+   
+});
+
+$("#desplegar3").click(function (e) {
+
+  e.preventDefault();
+
+  $(".m3__item").toggle();
+
+   
+});
+
+$("#desplegar4").click(function (e) {
+
+  e.preventDefault();
+
+  $(".m4__item").toggle();
+
+   
+});
+
+$("#desplegar5").click(function (e) {
+
+  e.preventDefault();
+
+  $(".m5__item").toggle();
+
+   
+});
+
+})
+
+
+
+
