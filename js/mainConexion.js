0cf0970c8de544f3136dee9860cacadb49acaaa7
Sol Ferri <solferri@hotmail.com>
2021-10-17 00:29:38 -0300

    actualización html conexión

---
+++ js/mainConexion.js
@@ -0,0 +1,116 @@
+$(document).ready(function(){
+  
+$("#desplegar").click(function (e) {
+
+  e.preventDefault();
+
+  $(".toggle").toggle();
+   
+});
+
+
+
+$("#desplegar2").click(function (e) {
+
+  e.preventDefault();
+
+  $(".toggle2").toggle();
+
+   
+});
+
+$("#desplegar3").click(function (e) {
+
+  e.preventDefault();
+
+  $(".toggle3").toggle();
+
+   
+});
+
+$("#desplegar4").click(function (e) {
+
+  e.preventDefault();
+
+  $(".toggle4").toggle();
+   
+});
+
+$("#desplegar5").click(function (e) {
+
+  e.preventDefault();
+
+  $(".toggle5").toggle();
+   
+});
+
+$("#desplegar6").click(function (e) {
+
+  e.preventDefault();
+
+  $(".toggle6").toggle();
+   
+});
+$("#desplegar7").click(function (e) {
+
+  e.preventDefault();
+
+  $(".toggle7").toggle();
+   
+});
+$("#desplegar8").click(function (e) {
+
+  e.preventDefault();
+
+  $(".toggle8").toggle();
+   
+});
+$("#desplegar9").click(function (e) {
+
+  e.preventDefault();
+
+  $(".toggle9").toggle();
+   
+});
+$("#desplegar10").click(function (e) {
+
+  e.preventDefault();
+
+  $(".toggle10").toggle();
+   
+});
+
+$("#desplegar11").click(function (e) {
+
+  e.preventDefault();
+
+  $(".toggle11").toggle();
+   
+});
+
+$("#desplegar12").click(function (e) {
+
+  e.preventDefault();
+
+  $(".toggle12").toggle();
+   
+});
+
+$("#desplegar13").click(function (e) {
+
+  e.preventDefault();
+
+  $(".toggle13").toggle();
+   
+});
+
+$("#desplegar14").click(function (e) {
+
+  e.preventDefault();
+
+  $(".toggle14").toggle();
+   
+});
+
+
+})
